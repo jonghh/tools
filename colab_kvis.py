390b1a23a18756ed06964878ddd096a1b33da3a1
jonghh <github>
2020-08-24 02:02:19 +0900

    2nd commit

--- colab_kvis.py
+++ colab_kvis.py
@@ -1,9 +1,13 @@
+import subprocess
+import matplotlib as mpl  # 기본 설정 만지는 용도
+import matplotlib.font_manager as fm  # 폰트 관련 용도
+
 def colab_kvis():
-    %config InlineBackend.figure_format = 'retina'
-    !apt -qq -y install fonts-nanum > /dev/null
+    subprocess.run(["config InlineBackend.figure_format = 'retina'"])
+    subprocess.run(["apt -qq -y install fonts-nanum > /dev/null"])
     path = "/usr/share/fonts/truetype/nanum/NanumBarunGothic.ttf"    # '/content/drive/My Drive/python_JH/Malgun-Gothic_29382.ttf'
     font = fm.FontProperties(fname=path, size=10)
     plt.rc('font', family=font.get_name())
     fm._rebuild()
     mpl.rcParams['axes.unicode_minus'] = False
-    return print("한글 시각화 준비 완료") 
+    return print("한글 시각화 준비 완료")
