c406d90839af0642d141684f6eaea359d09d664a
jonghh <github>
2020-08-24 00:50:50 +0900

    1st commit: colab에서 한글 폰트 깨지지 않게 시각화 준비

---
+++ colab_kvis.py
@@ -0,0 +1,9 @@
+def colab_kvis():
+    %config InlineBackend.figure_format = 'retina'
+    !apt -qq -y install fonts-nanum > /dev/null
+    path = "/usr/share/fonts/truetype/nanum/NanumBarunGothic.ttf"    # '/content/drive/My Drive/python_JH/Malgun-Gothic_29382.ttf'
+    font = fm.FontProperties(fname=path, size=10)
+    plt.rc('font', family=font.get_name())
+    fm._rebuild()
+    mpl.rcParams['axes.unicode_minus'] = False
+    return print("한글 시각화 준비 완료") 
