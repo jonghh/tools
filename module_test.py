2b0900608579094c822f362947b5904bb93fcafd
jonghh <github>
2020-09-08 21:42:55 +0900

    20200908_6

--- module_test.py
+++ module_test.py
@@ -2,7 +2,7 @@ class Person:    # 클래스
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        self.doc = ""
+        #self.doc = ""
 
     def greeting(self):
         print('안녕하세요. 저는 {0}입니다.'.format(self.name))
