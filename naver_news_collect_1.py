c435fe95297f1313bb032f2f1880196d3fcc4f07
jonghh <github>
2020-09-09 01:19:47 +0900

    20200908_15

--- naver_news_collect_1.py
+++ naver_news_collect_1.py
@@ -8,13 +8,14 @@ import pandas as pd
 
 class Naver_news_collect:
     def __init__(self, search_term,):
-        ''' 검색어, 검색 기간("20200321", "20200402" 방식), 저장할 경로(파일 이름 포함)'''
+        ''' 검색어 입력. 3 gram까지 가능 '''
         self.search_term = search_term
         #self.date_from = date_from
         #self.date_to = date_to
         #self.store_path = store_path
     def count(self, date_from, date_to, title):
-        ''' 검색 건수 보여주기 '''
+        ''' 검색 건수 보여주기.
+           검색 기간("20200321", "20200402" 방식)과 검색 영역(title=1(제목 검색), 0(전체 검색)) 지정 필요 '''
         if re.findall("\w\s\w\s*\w*", self.search_term):
             word = "".join(["%22", urllib.parse.quote_plus(self.search_term), "%22"])
         else:
@@ -24,11 +25,14 @@ class Naver_news_collect:
         req = requests.get(self.target_url)
         soup =BeautifulSoup(req.text, "html.parser")
         cases = soup.select_one("#main_pack > div.news.mynews.section._prs_nws > div.section_head > div.title_desc.all_my > span")
-        n_cases=int(cases.text.split("/")[1][:-1].replace(",","").strip())
-        self.cases = n_cases
-        return print("검색 결과, 총 {}건입니다".format(n_cases))
+        if cases:
+            n_cases=int(cases.text.split("/")[1][:-1].replace(",","").strip())
+            return print("검색 결과, 총 {}건입니다".format(n_cases))
+        else:
+            return print("검색 결과, 0건입니다")
+
     def collect1(self, store_path):
-        ''' 본문 제외 기사 수집 '''
+        ''' 본문 제외 기사 수집. 저장할 경로 지정 필요(파일 이름 포함) '''
         urlurl = [self.target_url]
         i = 1
         while self.cases > i+9:
@@ -56,7 +60,7 @@ class Naver_news_collect:
                         a2.append(re.findall(r'\d+\w+\s+전', ai)[0])
 
                 bbb = soup.select("dl > dt > a")
-                if len(bbb) > 10: bbb.remove(soup.select("dl > dt > a.link_help")[0]) 
+                if len(bbb) > 10: bbb.remove(soup.select("dl > dt > a.link_help")[0])
                 else: pass
                 b1 = [b.text for b in bbb]       # 제목
                 b2 = [b["href"] for b in bbb]    # URL
@@ -78,7 +82,7 @@ class Naver_news_collect:
         return self.news_all
 
     def collect2(self, store_path):
-        ''' 본문 포함 기사 수집 '''
+        ''' 본문 포함 기사 수집. 저장할 경로 지정 필요(파일 이름 포함) '''
         b3 = []
         for u in self.urls:
             try:
